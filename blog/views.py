7640a78f23195b42da9908df2c8780f3f8132fb4
pych1971 <pych1971@gmail.com>
2024-03-03 18:11:47 +0500

    2024-03-03-18-11

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Главная страница')
+    response = render_to_string('blog/index.html')
+    return HttpResponse(response)
 
 
 def posts(request):
