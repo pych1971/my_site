1efadd6a28a228f5a3b430cd010e99fedd16b204
pych1971 <pych1971@gmail.com>
2024-03-05 15:37:52 +0500

    2024-03-05-15-37

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,60 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
+data_post = [
+    {
+        'title': 'Рыбалка',
+        'description': 'Хорошо посидели',
+        'date': '21 авг 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'
+    },
+    {
+        'title': 'Париж',
+        'description': 'Незабываемое путешествие',
+        'date': '5 сент 2020',
+        'content': '''Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'''
+    },
+    {
+        'title': 'Финал лиги чемпионов',
+        'description': 'Реал опять выиграл ЛЧ',
+        'date': '28 мая 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, rem.'
+    },
+    {
+        'title': 'Охота на уток',
+        'description': 'Ни одна утка не пострадала',
+        'date': '7 дек 2019',
+        'content': 'Lorem ipsum dolor sit amet.'
+    },
+    {
+        'title': 'Фестиваль огурца',
+        'description': 'Суздаль ждет тебя',
+        'date': '12 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consectetur id inventore odit recusandae!'
+    },
+    {
+        'title': 'Нашествие',
+        'description': 'Даешь рок, но в следующем году',
+        'date': '29 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia recusandae rem?'
+    },
+    {
+        'title': 'Новый год',
+        'description': 'Эх, еще один год пролетел',
+        'date': '31 дек 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto corporis fuga ipsam laboriosam, nesciunt non quae qui ut veniam.'
+    },
+]
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'data_post': data_post
+    }
+    return render(request, 'blog/index.html', context=context)
 
 
 def posts(request):
